e65948c9c86c2f1a3a8b65d48a7a280c4e94a5c0
Pbbhootra19 <96310436+Pbbhootra19@users.noreply.github.com>
2022-11-11 22:08:48 +0530

    Add files via upload

---
+++ acronym.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+user_input = str(input("Enter a Phrase: "))
+text = user_input.split()
+a = " "
+for i in text:
+    a = a+str(i[0]).upper()
+print(a)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
